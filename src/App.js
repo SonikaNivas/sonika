6a9a0827058c1c5d38459789184e995fb864f586
SonikaNivas <727722euit179@skcet.ac.in>
2023-11-08 09:23:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './Components/NavBar';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <><NavBar></NavBar><Main></Main><Footer></Footer></>
+  );
+}
+
+export default App;
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <hr></hr>
+      <h2>xyz@gmail.com</h2>
+      <hr></hr>
+    </div>
+  );
+};
+
+export default Footer;
