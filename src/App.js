c00a8d92339fcd2579f94ab3a9234a92f5188613
SonikaNivas <727722euit179@skcet.ac.in>
2023-12-01 21:48:29 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-// App.js
 import React from 'react';
-import SignIn from './Project/SignIn';
-const App = () => {
-  return (
-        <>< SignIn/>
-        </>
-  );
-};
+import './App.css';
+import SignUp from './Project/SignUp';
 
+function App () {
+    return (
+        <><SignUp/></>
+    )
+}
 export default App;
+
